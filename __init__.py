be9a31066274492948343933678bfea8b9858786
Spyros <spyrostheodoridis@gmail.com>
2017-10-12 19:25:48 +0200

    version

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+__version__ = '0.1'
 from chorospy.chorospy.rasterFunc import getValuesAtPoint, getRasterValues, raster2array, array2raster, createRaster, filterByCoverage, clipRaster
 from chorospy.chorospy.vectorFunc import pointToGeo, disaggregate
 from chorospy.chorospy.bioFunc import makeDensityRaster
