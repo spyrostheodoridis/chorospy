da42ace8fe6bd02bd69ac8923fecf5a0c9a8db09
spyrostheodoridis <spyrostheodoridis@gmail.com>
2018-04-24 14:03:21 +0200

    reproject point

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ __version__ = '0.1'
 from chorospy.chorospy.rasterFunc import getValuesAtPoint, getRasterValues, raster2array, array2raster, createRaster, filterByCoverage, clipRaster
 from chorospy.chorospy.vectorFunc import pointToGeo, disaggregate, createFishNet
 from chorospy.chorospy.bioFunc import makeDensityRaster
-from chorospy.chorospy.transFunc import rasterToJSON
+from chorospy.chorospy.transFunc import rasterToJSON, reprojectPoint
